9d5300961edb36b5d55a54a21bb911d2786dc1ea
gradinafrica <gradinafrica@yahoo.com>
2014-01-13 15:39:19 -0800

    Added constructors, methods, and operator overloads for BaseInvariant

--- BaseInvariant/BaseInvariant.h
+++ BaseInvariant/BaseInvariant.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <vector>
+#include <deque>
 using namespace std;
 
 class BaseInvariant
 {
-	int m_base, m_decimalPosition;
-	vector<int> m_data;
+	unsigned int m_base, m_decimalPosition;
+	deque<int> m_data;
 	bool m_isNegative;
 
 public:
@@ -23,17 +23,93 @@ public:
 		m_data = other.m_data;
 		m_isNegative = other.m_isNegative;
 	}
-	BaseInvariant(int value)
+	BaseInvariant(int value, int base = 10)
 	{
-		m_base = 10;
+		m_base = base;
 		m_isNegative = value < 0;
 		m_decimalPosition = 0;
 		
 		do
 		{
 			m_decimalPosition++;
-			m_data.push_back(value % 10);
-			value /= 10;
+			m_data.push_front(value % m_base);
+			value /= m_base;
 		} while (value != 0);
+	}
+
+	int toInt()
+	{
+		int returnValue = 0;
+
+		for (unsigned int i = 0; i < m_data.size() && i < m_decimalPosition; i--)
+			returnValue += m_data[i] * (int) pow(m_base, m_decimalPosition - i - 1);
+
+		return m_isNegative ? -returnValue : returnValue;
+	}
+	double toDouble()
+	{
+		double returnValue = 0;
+
+		for (unsigned int i = 0; i < m_data.size(); i++)
+			returnValue += (double) m_data[i] * pow(m_base, m_decimalPosition - i - 1);
+
+		return m_isNegative ? -returnValue : returnValue;
+	}
+	void changeBase(int newBase)
+	{
+		double asDouble = abs(toDouble());
+		int asInt = (int) floor(asDouble);
+		double decimalPart = asDouble - (double) asInt;
+
+		m_data.clear();
+		m_base = newBase;
+
+		do
+		{
+			m_data.push_front(asInt % m_base);
+			asInt /= m_base;
+		} while (asInt != 0);
+
+		double currentPower = 1.0 / m_base;
+		m_decimalPosition = m_data.size();
+
+		do
+		{
+			if (decimalPart > 0)
+				m_data.push_back((int) floor(decimalPart /= currentPower /= m_base));
+		} while (decimalPart > 0);
+	}
+
+	bool operator==(BaseInvariant& rhs)
+	{
+		return m_data == rhs.m_data && m_base == rhs.m_base && m_isNegative == rhs.m_isNegative;
+	}
+	bool operator!=(BaseInvariant& rhs)
+	{
+		return !(*this == rhs);
+	}
+	BaseInvariant& operator=(BaseInvariant& rhs)
+	{
+		m_data = rhs.m_data;
+
+		changeBase(m_base);
+
+		return *this;
+	}
+	BaseInvariant operator+(BaseInvariant& rhs)
+	{
+		return BaseInvariant((int) (toDouble() + rhs.toDouble()));
+	}
+	BaseInvariant operator-(BaseInvariant& rhs)
+	{
+		return BaseInvariant((int) (toDouble() - rhs.toDouble()));
+	}
+	BaseInvariant& operator+=(BaseInvariant& rhs)
+	{
+		return *this = *this + rhs;
+	}
+	BaseInvariant& operator-=(BaseInvariant& rhs)
+	{
+		return *this = *this - rhs;
 	}
 };
