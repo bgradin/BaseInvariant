06c2fd507c67aa04f75fceeb0775e2a4a48a5e82
gradinafrica <gradinafrica@yahoo.com>
2014-01-14 10:25:44 -0800

    Removed unnecessary test case

--- BaseInvariant/main.cpp
+++ BaseInvariant/main.cpp
@@ -15,11 +15,6 @@ void main()
 	test2 *= 2;
 	test3 /= 2;
 
-	// As you can see above, numbers can be directly referenced when used in equations.
-	//   As demonstrated below, for use in expressions, numbers must be converted to
-	//   BaseInvariant through use of the base10(a) macro.
-	int test4 = (int) (test1 - 0.25);
-
 	// Output numbers as a specific data type, or in their current base
 	// - Numbers in a specific base will be outputted with spaces in between each digit.
 	//   Otherwise, it's impossible to tell where one digit ends and the next begins
