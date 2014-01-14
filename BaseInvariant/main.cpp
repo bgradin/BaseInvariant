99af219e9c33ea5b9f5debef3a76487711159ced
gradinafrica <gradinafrica@yahoo.com>
2014-01-14 02:45:28 -0800

    Supposed to be in last commit.

--- BaseInvariant/main.cpp
+++ BaseInvariant/main.cpp
@@ -15,11 +15,16 @@ void main()
 	test2 *= 2;
 	test3 /= 2;
 
+	// As you can see above, numbers can be directly referenced when used in equations.
+	//   As demonstrated below, for use in expressions, numbers must be converted to
+	//   BaseInvariant through use of the base10(a) macro.
+	int test4 = (int) (test1 - base10(0.25));
+
 	// Output numbers as a specific data type, or in their current base
 	// - Numbers in a specific base will be outputted with spaces in between each digit.
 	//   Otherwise, it's impossible to tell where one digit ends and the next begins
-	cout << "test1(int)    = " << test1.toInt() << endl;
-	cout << "test1(double) = " << test1.toDouble() << endl;
+	cout << "test1(int)    = " << (int) test1 << endl;
+	cout << "test1(double) = " << (double) test1 << endl;
 	cout << "test2         = " << test2 << endl;
-	cout << "test3 - 13    = " << test3 - 13 << endl;
+	cout << "test3 - 13    = " << test3 - base10(13) << endl;
 }
